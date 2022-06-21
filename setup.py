bf0fafce085c1556ffb735c16c7a2fb1c63f959b
anomalia <anavgo@gmail.com>
2022-06-21 13:06:17 +0200

    increase version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import os
 NAME = "Orange3-MECODA"
 DOCUMENTATION_NAME = 'Orange MECODA'
 
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 
 AUTHOR = "Ana Alvarez, ICM-CSIC"
 AUTHOR_EMAIL = "ana.alvarez@icm.csic.es"
